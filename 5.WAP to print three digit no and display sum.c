3398935238748b04b73e4e75ded300d57c2b2465
sumch76 <108131586+sumch76@users.noreply.github.com>
2022-10-21 12:41:58 +0530

    Update 5.WAP to print three digit no and display sum.c

--- 5.WAP to print three digit no and display sum.c
+++ 5.WAP to print three digit no and display sum.c
@@ -7,3 +7,16 @@ int main()
     sum=x/100+x/10%10+x%10;
     printf("%d",sum);
 }
+or
+#include<stdio.h>
+int main()
+{
+int x,sum;
+printf("enter a numbr:");
+scanf("%d",&x);
+while(x)
+{
+sum=sum+x%10;
+x=x/10;
+printf("sum of digit is %d",sum);
+}
